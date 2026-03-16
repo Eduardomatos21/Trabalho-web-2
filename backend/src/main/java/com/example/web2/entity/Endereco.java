d0052aaf22af7107eaf7325271f0f44ba9b6ff8d
solanoraul <solano.rr@gmail.com>
2026-03-16 18:33:00 -0300

    getters and setters equipamento e endereço

--- backend/src/main/java/com/example/web2/entity/Endereco.java
+++ backend/src/main/java/com/example/web2/entity/Endereco.java
@@ -28,6 +28,62 @@ public class Endereco {
     @OneToMany(mappedBy = "endereco")
     private List<Cliente> clientes;
 
+    public Integer getId () {
+        return id; 
+    }
+
+    public Integer getCep () {
+        return cep; 
+    }
+
+    public String getRua () {
+        return rua; 
+    }
+
+    public String getBairro () {
+        return bairro; 
+    }
+
+    public Integer getNumero () {
+        return numero; 
+    }
+
+    public String getCidade () {
+        return cidade; 
+    }
+    
+    public String getPais () {
+        return pais; 
+    }
+
+    public void setId (Integer id) {
+        this.id = id;
+    }
+
+    public void setCep (Integer cep) {
+        this.cep = cep;
+    }
+
+    public void setRua (String rua) {
+        this.rua = rua;
+    }
+
+    public void setBairro (String bairro) {
+        this.bairro = bairro;
+    }  
+    
+    public void setNumero (Integer numero) {
+        this.numero = numero;
+    }
+
+    public void setCidade (String cidade) {
+        this.cidade = cidade;
+    }  
+
+    public void setPais (String pais) {
+        this.pais = pais;
+    }  
+
 
     public Endereco() {}
 
