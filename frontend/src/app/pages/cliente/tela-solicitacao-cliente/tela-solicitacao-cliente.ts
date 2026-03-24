20a7bea000c19df531ac1a50605569880246625b
eduardo <eduardocostamatos@hotmail.com>
2026-03-24 15:19:34 -0300

    criado componente tela solicitação cliente

---
+++ frontend/src/app/pages/cliente/tela-solicitacao-cliente/tela-solicitacao-cliente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-solicitacao-cliente',
+  imports: [],
+  templateUrl: './tela-solicitacao-cliente.html',
+  styleUrl: './tela-solicitacao-cliente.css',
+})
+export class TelaSolicitacaoCliente {}
