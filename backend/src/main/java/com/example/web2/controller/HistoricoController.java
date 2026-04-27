c46688f56053b2154d1369295f675f37e3b59fe8
leoyuiti <leonardoyuiti@ufpr.br>
2026-04-27 12:13:06 -0300

    HistoricoService e Controller ajustados

--- backend/src/main/java/com/example/web2/controller/HistoricoController.java
+++ backend/src/main/java/com/example/web2/controller/HistoricoController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,23 +16,36 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.web2.entity.Historico;
-import com.example.web2.repository.HistoricoRepository;
+import com.example.web2.service.HistoricoService;
 
 @RestController
 @RequestMapping("/historico")
+@Validated
+@CrossOrigin(origins = "http://localhost:4200")
 public class HistoricoController {
 
     @Autowired
-    private HistoricoRepository historicoRepository;
+    private HistoricoService historicoService;
 
     @GetMapping
     public List<Historico> listar() {
-        return historicoRepository.findAll();
+        return historicoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Historico buscar(@PathVariable Integer id) {
+        return historicoService.buscarPorId(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Historico adicionar(@RequestBody Historico historico) {
-        return historicoRepository.save(historico);
+        return historicoService.salvar(historico);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        historicoService.deletar(id);
     }
 }
