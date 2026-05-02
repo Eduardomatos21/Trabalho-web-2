eb56cb60cebc28cd5e785b3c931d740070aaaa27
eduardo <eduardocostamatos@hotmail.com>
2026-05-02 14:41:30 -0300

    corrigido erro ao compilar, SolicitacaoFuncionarioController estava fazendo GetMapping do /abertas 2 vezes

--- backend/src/main/java/com/example/web2/controller/SolicitacaoFuncionarioController.java
+++ backend/src/main/java/com/example/web2/controller/SolicitacaoFuncionarioController.java
@@ -88,24 +88,6 @@ public class SolicitacaoFuncionarioController {
         return token;
     }
 
-    private final SolicitacaoFuncionarioService service;
-
-    public SolicitacaoFuncionarioController(SolicitacaoFuncionarioService service) {
-        this.service = service;
-    }
-
-    @GetMapping("/abertas")
-    public List<Solicitacao> listarAbertas() {
-        return service.buscarAbertas();
-    }
-
-
-
-
-
-
-
-
 
     
 }
